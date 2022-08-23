4716cd037fbc1c5d56f41564f6a9a68f5a44c859
pascal sene <iron82@outlook.fr>
2022-08-23 18:00:09 +0200

    co,ntroller_reviews

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
+  validates :email, presence: true, format: { with: /\A\S*@\w*\.[a-zA-Z]{2,3}\z/ }
+  validates_presence_of :lastname
+  validates :firstname, presence: true, uniqueness: { scope: :lastname }
+  validates :photo, presence: true
+  validates :encrypted_password, presence: true
   has_many :bookings, dependent: :destroy
   has_many :cars
   has_many :reviews, through: :bookings
-  validates :email, presence: true, format: { with: // }
-  validates_presence_of :lastname
-  validates :firstname, presence: true, uniqueness: { scope: :lastname }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable, :trackable
 end
