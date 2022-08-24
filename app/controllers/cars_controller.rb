667bdd5e5c8c09ce52d73f28c8d7a71322f004e0
abdillah attoumani <a.attoumani13@outlook.fr>
2022-08-24 16:52:03 +0200

    correct5

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -57,6 +57,10 @@ class CarsController < ApplicationController
   end
 
   def destroy
+    @car.bookings.each do |booking|
+      booking.review.destroy
+    end
+    @car.bookings.destroy_all
     @car.destroy
     redirect_to root_path, status: :see_other, notice: "Your car has been Destroyed"
   end
