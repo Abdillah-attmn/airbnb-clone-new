ab265a518a9f57ffee2e823d500c241568237cd9
abdillah attoumani <a.attoumani13@outlook.fr>
2022-08-25 11:22:04 +0200

    user

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,8 +1,10 @@
 class Booking < ApplicationRecord
+  PENDING = { pending_guest: "Pending guest request",
+              pending_host: "Pending host validation",
+              confirmed: "Confirmed", canceled: "Canceled" }
   belongs_to :user
   belongs_to :car
   has_one :review
   validates_presence_of :start_date
   validates_presence_of :end_date
-  validates :status, inclusion: { in: ["Pending guest request", "Pending host validation", "Confirmed", "Canceled"], allow_nil: true }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_24_094427) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_24_211602) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,7 +23,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_094427) do
     t.datetime "updated_at", null: false
     t.integer "value"
     t.string "status"
-    t.integer "price"
     t.index ["car_id"], name: "index_bookings_on_car_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
