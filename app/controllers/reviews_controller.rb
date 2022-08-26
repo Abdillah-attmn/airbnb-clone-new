9aa2723e3f9a53b0369ddb95090d574a91382042
abdillah attoumani <a.attoumani13@outlook.fr>
2022-08-26 15:30:32 +0200

    carbnbv2.3

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,12 +3,15 @@ class ReviewsController < ApplicationController
   def new
     @review = Review.new
   end
-  
+
   def create
     @review = Review.new(review_params)
     @review.booking = @booking
-    @review.save
-    redirect_to bookings_path
+    if @review.save!
+      redirect_to bookings_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,37 +5,46 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
 require 'faker'
 Review.destroy_all
 Booking.destroy_all
 Car.destroy_all
 User.destroy_all
 
-5.times do
+files = %w[https://www.largus.fr/images/images/porsche_911-black-edition-2015.jpg https://www.wrapstyle.com/content/img_cache/1920x/1594897977-1171-GMC-Sierra-Black-Matt-1-.jpg https://sf2.autoplus.fr/wp-content/uploads/autoplus/2021/11/67-ferrari-daytona-sp3-1.jpg https://images.caradisiac.com/images/3/1/9/2/193192/S0-nouveau-suv-electrification-integration-au-groupe-stellantis-ou-va-maserati-697186.jpg https://cdn.drivek.it/configurator-imgs/cars/de/800/CHEVROLET/CAMARO/6994_COUPE-2-TURER/camaro-coupe-2016-front-2.jpg ]
+cars_name = %w[porsche sierra ferrari maserati camaro]
+
+i = 0
+while i < 5 do
   user = User.create!(firstname: Faker::Name.first_name, lastname: Faker::Name.last_name,
                       email: Faker::Internet.email, photo: Faker::Avatar.image,
-                      password: "password")
-  2.times do
-    car = Car.create!(title: Faker::Vehicle.make_and_model, content: Faker::Vehicle.fuel_type,
-                      photo: "https://source.unsplash.com/random/?car",
-                      address: Faker::Address.full_address, latitude: Faker::Address.latitude,
-                      longitude: Faker::Address.longitude, price: (120..130).to_a.sample,
-                      user: user)
+                      password: "password"
+  )
+  # p files[i]
+  p file = URI.open( files[i] )
 
+  car = Car.new(title: cars_name[i], content: Faker::Vehicle.fuel_type,
+                address: "11 rue benjamin franklin 77000 La Rochette", latitude: Faker::Address.latitude,
+                longitude: Faker::Address.longitude, price: (120..130).to_a.sample,
+                user: user)
+  car.photocar.attach(io: file, filename: "#{file}.jpg", content_type: "image/jpg")
+  car.save!
 
-    booking = Booking.new(user: car.user,
-                              start_date: Faker::Date.in_date_period,
-                              end_date: Faker::Date.in_date_period,
-                              status: ["Pending guest request", "Pending host validation",
-                                       "Confirmed", "Canceled"].sample,
-                              car: car)
+  booking = Booking.new(user: car.user,
+                        start_date: Faker::Date.in_date_period,
+                        end_date: Faker::Date.in_date_period,
+                        status: ["Pending guest request", "Pending host validation",
+                                 "Confirmed", "Canceled"].sample,
+                        car: car)
 
-    if (booking[:end_date] - booking[:start_date]) >= 0
-      booking[:value] = car.price * (booking[:end_date] - booking[:start_date])
-    else
-      booking[:value] = -1
-    end
-    booking.save!
-    Review.create!(rating: (0..5).to_a.sample, content: "It's good", booking: booking)
+  if (booking[:end_date] - booking[:start_date]) >= 0
+    booking[:value] = car.price * (booking[:end_date] - booking[:start_date])
+  else
+    booking[:value] = -1
   end
+  booking.save!
+  Review.create!(rating: (0..5).to_a.sample, content: "It's good", booking: booking)
+
+  i += 1
 end
